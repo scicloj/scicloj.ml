9d04483424da6173613c70b405a9c3fbc0dce476
Carsten Behring <carsten.behring@gmail.com>
2021-05-31 14:14:10 +0200

    new chapter on linear regressions

--- userguide/scicloj/ml/models.clj
+++ userguide/scicloj/ml/models.clj
@@ -54,16 +54,122 @@ and [Xgboost](https://xgboost.readthedocs.io/en/latest/jvm/index.html)"]
 ^kind/hiccup-nocode
 (render-key-info ":xgboost")
 
+["# Example code for models"]
 
-["## Random Forrest"]
 (require '[scicloj.ml.core :as ml]
          '[scicloj.ml.metamorph :as mm]
          '[scicloj.ml.dataset :refer [dataset add-column] ]
          '[scicloj.ml.dataset :as ds]
          '[tech.v3.dataset.math :as std-math]
          '[tech.v3.datatype.functional :as dtf]
-
+         '[scicloj.metamorph.ml.toydata :as datasets]
          )
+
+["## Linear regression"]
+
+["In this example we will explore the relationship between the
+body mass index (bmi) and a diabetes indicator."]
+
+["First we load the data and split into train and test sets."]
+(def diabetes (datasets/diabetes-ds))
+
+(def diabetes-train
+  (ds/head diabetes 422))
+
+(def diabetes-test
+  (ds/tail diabetes 20))
+
+
+
+["Next we create the pipeline, converting the target variable to
+a float value, as needed by the model."]
+
+(def pipe-fn
+  (ml/pipeline
+   (mm/select-columns [:bmi :disease-progression])
+   (mm/update-column :disease-progression (fn [col] (map #(double %) col)))
+   (mm/convert-types :disease-progression :float32)
+   (mm/set-inference-target :disease-progression)
+   {:metamorph/id :model} (mm/model {:model-type :smile.regression/ordinary-least-square})))
+
+["We can then fit the model, by running the pipeline in mode :fit"]
+
+(def fitted
+  (ml/fit
+   diabetes-train
+   pipe-fn))
+
+
+["Next we run the pipe-fn in :transform and extract the prediction
+for the disease progression:"]
+(def diabetes-test-prediction
+  (-> diabetes-test
+      (ml/transform pipe-fn fitted)
+      :metamorph/data
+      :disease-progression))
+
+["The truth is available in the test dataset."]
+(def diabetes-test-trueth
+  (-> diabetes-test
+      :disease-progression
+      ))
+
+
+
+["The smile Java object of the LinearModel is in the pipeline as well:"]
+
+(def model-instance
+  (-> fitted
+      :model
+      (ml/thaw-model)))
+
+["This object contains all information regarding the model fit
+such as coefficients and formula:"]
+(-> model-instance .coefficients seq)
+(-> model-instance .formula str)
+
+["Smile generates as well a String with the result of the linear
+regression as part of the toString() method of class LinearModel:"]
+^kind/code
+(println-str
+ (str model-instance))
+
+["This tells us that there is a statistically significant
+(positive) correlation between the bmi and the diabetes
+disease progression in this data."]
+
+
+["At the end we can plot the truth and the prediction on the test data,
+and observe the linear nature of the model."]
+
+^kind/vega
+{:layer [
+         {:data {:values (map #(hash-map :disease-progression %1 :bmi %2 :type :trueth)
+                                 diabetes-test-trueth
+                                 (:bmi  diabetes-test))}
+
+          :width 500
+          :height 500
+          :mark {:type "circle"}
+          :encoding {:x {:field :bmi :type "quantitative"}
+                     :y {:field :disease-progression :type "quantitative"}
+                     :color {:field :type}}}
+
+         {:data {:values (map #(hash-map :disease-progression %1 :bmi %2 :type :prediction)
+                  diabetes-test-prediction
+                  (:bmi diabetes-test))}
+
+          :width 500
+          :height 500
+          :mark {:type "line" }
+          :encoding {:x {:field :bmi :type "quantitative"}
+                     :y {:field :disease-progression :type "quantitative"}
+                     :color {:field :type}}}
+
+         ]}
+
+["## Random Forrest"]
+
 
 
 ["The following code plots the decision surfaces of the random forest
