10da11c344540c691afed12200608f1a5cc1de03
Carsten Behring <carsten.behring@gmail.com>
2021-10-22 16:37:12 +0200

    using build.clj

---
+++ build.clj
@@ -0,0 +1,34 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
+            [org.corfield.build :as bb]))
+
+(def lib 'scicloj/scicloj.ml)
+; alternatively, use MAJOR.MINOR.COMMITS:
+;; (def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version "0.1.1-SNAPSHOT")
+
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version version :aliases [:test-runner])
+      (bb/run-tests)
+      (bb/clean)
+      (bb/jar)))
+
+(defn ci-no-test "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/clean)
+      (bb/jar)))
+(defn install "Install the JAR locally." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/install)))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
