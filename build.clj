d57c0d48d2134ab6047f44f5b53d2093331af5d9
Carsten Behring <carsten.behring@efsa.europa.eu>
2021-12-24 19:46:44 +0100

    increase smile verion

--- build.clj
+++ build.clj
@@ -5,8 +5,8 @@
 
 (def lib 'scicloj/scicloj.ml)
 ; alternatively, use MAJOR.MINOR.COMMITS:
-;; (def version (format "1.0.%s" (b/git-count-revs nil)))
-(def version "0.1.1")
+(def version (format "0.2.%s" (b/git-count-revs nil)))
+;; (def version "0.1.2")
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
